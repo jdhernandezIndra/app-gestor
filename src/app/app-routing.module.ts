c74c90015a5b352e42eb958720f25f41f37ec572
jdhernandez <jdhernandez@indracompany.com>
2023-03-06 16:53:10 -0500

    Ajustando componentes principales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './ingreso/login/login.component';
+import { RegisterComponent } from './ingreso/register/register.component';
+import { NopageFoundComponent } from './nopage-found/nopage-found.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { PaginasComponent } from './pages/paginas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PaginasComponent,
+    children: [
+      { path: 'inicio', component: InicioComponent },
+      { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+    ]
+  },
+
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+
+  { path: '**', component: NopageFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { LoginComponent } from './ingreso/login/login.component';
+import { RegisterComponent } from './ingreso/register/register.component';
+
+import { NopageFoundComponent } from './nopage-found/nopage-found.component';
+import { PaginasComponent } from './pages/paginas.component';
+import { MenuComponent } from './pages/principal/menu/menu.component';
+import { HeaderComponent } from './pages/principal/header/header.component';
+import { FooterComponent } from './pages/principal/footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NopageFoundComponent,
+    InicioComponent,
+    PaginasComponent,
+    MenuComponent,
+    HeaderComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
